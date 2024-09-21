2eb40e2973ae241accb637758a71d1a26026875b
jnpyyearth <jetnipat.y@ku.th>
2024-09-21 13:22:24 +0700

    edit

---
+++ src/app/Manager/add-tour/add-tour.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-tour',
+  templateUrl: './add-tour.component.html',
+  styleUrl: './add-tour.component.css'
+})
+export class AddTourComponent {
+
+}
---
+++ src/app/Manager/home-manager/home-manager.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-manager',
+  templateUrl: './home-manager.component.html',
+  styleUrl: './home-manager.component.css'
+})
+export class HomeManagerComponent {
+
+}
